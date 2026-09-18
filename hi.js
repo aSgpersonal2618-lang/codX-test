f3c875c0cd6aa2a9fe9a6231c4b52072a458989c
harxhtttt <jhah0648@gmail.com>
2026-09-18 12:31:05 +0530

    Add function to find duplicate emails

--- hi.js
+++ hi.js
@@ -1,1 +1,12 @@
 // hi
+function findDuplicateEmails(newSignups, existingUsers) {
+  const duplicates = [];
+  for (let i = 0; i < newSignups.length; i++) {
+    for (let j = 0; j < existingUsers.length; j++) {
+      if (newSignups[i].email === existingUsers[j].email) {
+        duplicates.push(newSignups[i].email);
+      }
+    }
+  }
+  return duplicates;
+}
