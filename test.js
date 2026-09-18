df5812b16665010248b398f099b4a02134a6bfdb
aSgpersonal2618-lang <aryanpersonal2618@gmail.com>
2026-09-18 13:47:58 +0530

    Update test.js1

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
 console.log('js');
+function highComplexityLoop(n) {
+  let count = 0;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      for (let k = 0; k < n; k++) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
+
+console.log(highComplexityLoop(100)); // 100^3 = 1,000,000 iterations
